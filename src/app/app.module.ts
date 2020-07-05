08569b341033a7701e35cea4b5f5850af8e2e882
NaraGuimma <naraguimma@gmail.com>
2020-07-05 09:18:30 -0300

    incluindo o component profissionais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { PublicacaoComponent } from './publicacao/publicacao.component';
+import { ProfissionaisComponent } from './profissionais/profissionais.component';
 
 @NgModule({
   declarations: [
@@ -15,17 +15,18 @@ import { PublicacaoComponent } from './publicacao/publicacao.component';
     PrincipalComponent,
     CadastroComponent,
     LoginComponent,
-    PublicacaoComponent
+    PublicacaoComponent,
+    ProfissionaisComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
-    {path: 'login', component: LoginComponent},
-    {path: 'principal', component: PrincipalComponent},
+      { path: 'login', component: LoginComponent },
+      { path: 'principal', component: PrincipalComponent },
     ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
