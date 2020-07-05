6c3636b295f615e4f1074f265b8c7a0d556f5216
flavio <flavio.diminuto@outlook.com>
2020-07-05 14:54:23 -0300

    adicao do top-menu no login e tela principal

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,6 @@ import { FormControl } from '@angular/forms';
 import { Conta } from '../model/conta';
 import { CONTAMOCK } from '../mock/mock-profissionais';
 import { Router, ActivatedRoute } from '@angular/router';
-import { MainLogoComponent } from '../main-logo/main-logo.component';
 
 @Component({
   selector: 'app-login',
@@ -14,23 +13,28 @@ export class LoginComponent implements OnInit {
   contas : Conta[];
   email = new FormControl();
   senha = new FormControl();
+  
   constructor(private router : Router, private routes : ActivatedRoute ) { }
 
   ngOnInit(): void {
     //carrega contas
     this.contas = CONTAMOCK;
+    console.log(this.contas);
   }
 
   login() {
+    let valido = false;
     this.contas.forEach(conta => {
       if(conta.email == this.email.value
       && conta.senha == this.senha.value){
-        console.log('ACHEI A CONTA ! ! !')
+        valido = true;
         this.router.navigate(['/principal']);
       }
     });
-  
-    alert("Login ou senha invalida!")
+    
+    if(!valido){
+      alert("Login ou senha invalida!")
+    }
   }
 
 }
