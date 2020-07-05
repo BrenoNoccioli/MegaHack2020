275eceeabb975625526aa2ebd1e3a357cdec13fb
flavio <flavio.diminuto@outlook.com>
2020-07-05 20:04:10 -0300

    Ajuste menus

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-principal',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalComponent implements OnInit {
 
-  constructor() { }
+  id : string;
+  constructor( private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
+    
+  this.id = this.route.snapshot.params["id"];
+
   }
+  
 
 }
--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-top-menu',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-menu.component.css']
 })
 export class TopMenuComponent implements OnInit {
+  //links
+  principal: string;
+  cadastro: string;
+  lista_prof:string;
+  publicacoes: string;
+  sair : string;
+
+  id : string;
  logado : boolean;
-  constructor() { }
+  constructor( private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
+    this.principal = `/principal/${this.id}`;
+    this.cadastro = `/cadastro`;
+    this.lista_prof = `/profissionais/${this.id}`;
+    this.publicacoes = `/publicacoes`;
+    
+  this.id = this.route.snapshot.params["id"];
     this.logado = false;
   }
   
