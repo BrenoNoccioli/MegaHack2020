037ef2424e3ed9589ee91817eeb532777b9bf749
flavio <flavio.diminuto@outlook.com>
2020-07-05 09:44:27 -0300

    elementos da tela de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Conta } from '../model/conta';
+import { CONTAMOCK } from '../mock/mock-profissionais';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,25 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  contas = CONTAMOCK;
+
   email = new FormControl();
   senha = new FormControl();
   constructor() { }
 
   ngOnInit(): void {
+    //carregar contas
+  }
+
+  login() {
+    this.contas.forEach(conta => {
+      if(conta.email == this.email.value
+      && conta.senha == this.senha.value){
+        console.log('ACHEI A CONTA ! ! !')
+      }else{
+        console.log('NÃO ACHEI ! !')
+      }
+    });
   }
 
 }
