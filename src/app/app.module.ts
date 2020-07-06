147fe324971271c9d582b92af4daa510ee687d6d
flavio <flavio.diminuto@outlook.com>
2020-07-05 23:40:03 -0300

    Disponibilizando as publicacoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { TopMenuComponent } from './top-menu/top-menu.component';
     {path: 'cadastro', component: CadastroComponent},
     {path: 'cadastro/:id', component: CadastroComponent},
     {path: 'publicacao', component: PublicacaoComponent},
+    {path: 'publicacao/:id', component: PublicacaoComponent},
     {path: 'profissionais/:id', component: ProfissionaisComponent},
     { path: '',   redirectTo: '/login', pathMatch: 'full'},
     { path:'**'   , component: LoginComponent  }
